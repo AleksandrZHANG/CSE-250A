2fe8b3586a66a3594406e8aebd1a436cf28f231c
Hao Gai <hgai@Haos-MBP-2.attlocal.net>
2016-05-13 20:49:07 -0700

    max_freq_modified

--- DictionaryTrie.hpp
+++ DictionaryTrie.hpp
@@ -90,7 +90,7 @@ private:
         dfs(prefix, node->right, pq, num);
         if (!pq.empty()) {
             if ((pq.size() < num)||(node->max_freq > pq.top().first)) {
-                dfs(prefix, node->middle, pq, num);
+                dfs(tmp, node->middle, pq, num);
             }
         }
     }
